3e9af36038ed3f6e1f00083e4ec37d7862899f09
Mennaesmael <114106408+mennaesmael@users.noreply.github.com>
2024-07-18 19:43:29 +0300

    set up the next project and components

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require('path');
+const nextConfig = {
+        webpack: (config) => {
+          config.resolve.alias['@'] = path.join(__dirname, 'src');
+          return config;
+        },
+};
+
 
 export default nextConfig;
